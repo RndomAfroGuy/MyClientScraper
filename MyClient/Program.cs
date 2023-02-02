26443c333c42e86528a687ccf29b6ba90a94a82c
Emanuel Ribeiro <e.ribeiro@ua.pt>
2023-02-02 00:45:11 +0000

    Add project files.

---
+++ MyClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyCodeLibrary;
+
+namespace Myclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape scrape = new Scrape();
+            string thescraped = scrape.ScrapeWebpage("https://google.com");
+            Console.WriteLine(thescraped);
+
+            string filepath = @"C:\Users\emanr\OneDrive\Desktop\C#\newExample.txt";
+            string thescrapedwithfile = scrape.ScrapeWebpage("https://google.com",filepath);
+            Console.WriteLine("you file has succesfully been copyed to your filepath of choice {0}",filepath );
+            Console.ReadLine();
+        }
+    }
+}
